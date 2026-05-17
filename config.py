3788a29955080a6d4a392620ece5660e5bbc8289
carlo-perini <carlo.perini@3asport.it>
2026-05-17 14:42:26 +0200

    landing subtitle as quote

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ WALL_ROW_COLORS = [
 ]
 
 # Frase di benvenuto nella pagina iniziale
-LANDING_SUBTITLE = "Benevenuti a Only connect. Se i quiz fossero ostacoli da superare, le domande che potete trovare in quiz prima di cena sarebbero lo scalino prima del portone. Le domande di Only connect la cordigliera delle Ande. Per il lungo."
+LANDING_SUBTITLE = "Se i quiz fossero ostacoli da superare, le domande che potete trovare in un quiz orario pre-cena sarebbero lo scalino prima del portone di casa. Le domande di Only connect, invece, la cordigliera delle Ande. Per il lungo."
 
 # Modalità debug
 DEBUG = os.getenv("FLASK_ENV") == "development"
